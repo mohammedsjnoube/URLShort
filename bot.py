76275299e222448450e3c342904b9a63c087caf5
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-12-06 16:21:18 +0530

    added access token and guid

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from pyrogram import Client
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+GUID = os.environ.get("GUID")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 
 bot = Client(":memory:",
              api_id=API_ID,
