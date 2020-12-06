181f757d2ab1685510eb85dc9ea66571bf5d3d37
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-12-06 16:49:50 +0530

    Update bitly.py

--- bitly.py
+++ bitly.py
@@ -19,3 +19,8 @@ async def reply_bitly_link(_, msg: Message):
         await msg.reply("`Provide Valid Link.`")
     else:
         await msg.reply(f"**Shorten link:** `{short_url}`")
+
+
+if __name__ == "__main__":
+    bot.run()
+
