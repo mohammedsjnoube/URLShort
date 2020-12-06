d6be938d1b7b36a9e93231ab516cc51b6a4f3c13
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-12-06 16:28:29 +0530

    added Start Message.

--- bot.py
+++ bot.py
@@ -8,6 +8,13 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 GUID = os.environ.get("GUID")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 
+START_MESSAGE = """ Hello {},
+`I am Bitly Bot to shorten links.
+Send a valid link to shorten.`
+
+Made with ❤️ by [Krishna](https://t.me/Krishna_Singhal).
+"""
+
 bot = Client(":memory:",
              api_id=API_ID,
              api_hash=API_HASH,
