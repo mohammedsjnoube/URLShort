6b68274f72f2dedb85cf944f81c7cc6036273940
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-12-06 17:50:47 +0530

    some changes

--- bitly_func.py
+++ bitly_func.py
@@ -8,6 +8,7 @@ class bitly():
     def __init__(self, link):
         self.link = link
         self.response = True
+        self.error = None
 
     def convert_url(self) -> Optional[str]:
         long_url = self.link
@@ -17,8 +18,11 @@ class bitly():
         shorten_res = requests.post(
             url, json={"group_guid": GUID, "long_url": long_url}, headers=headers
         )
-        if shorten_res.status_code == 200:
-            short_url = shorten_res.json().get("link")
+        if shorten_res.status_code == 400:
+            if "INVALID_ARG_LONG_URL" in shorten_res.json().get("message"):
+                self.response, self.error = False, shorten_res.json().get("description")
+            else:
+                self.response = False
         else:
-            self.response = False
+            short_url = shorten_res.json().get("link")
         return short_url
