58fff55249e440f7179c0645092fa1ae6f170b48
BlackStoneOfficial <70601966+BlackStoneOfficial@users.noreply.github.com>
2020-12-28 19:11:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 GUID = os.environ.get("GUID")
-ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN = os.environ.get("BITLY_TOKEN")
 
 START_MESSAGE = """ Heya {},
 
