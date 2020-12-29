372a45cc74cb618905fb48a678f1420c08ed3eb3
BlackStoneOfficial <70601966+BlackStoneOfficial@users.noreply.github.com>
2020-12-29 10:44:39 +0100

    Update url_func.py

--- url_func.py
+++ url_func.py
@@ -40,8 +40,26 @@ class t1p():
         url = f'https://s-url.eu/api/url-short/?url={long_url}&srv=t1p'
         shorten_res = requests.post(url)
         if shorten_res.status_code == 400:
-            self.response = False
-            self.error = False
+            self.response, self.error = False, "INVALID URL!"
+        else:
+            short_url = shorten_res.json().get("shortened")
+        return short_url
+
+    
+class ogy():
+    def __init__(self, link):
+        self.link = link
+        self.response = True
+        self.error = None
+
+    def convert_url(self) -> Optional[str]:
+        get_long = self.link
+        long_url = get_long.replace("/ogy ", "")
+        short_url = None
+        url = f'https://s-url.eu/api/url-short/?url={long_url}&srv=ogy'
+        shorten_res = requests.post(url)
+        if shorten_res.status_code == 400:
+            self.response, self.error = False, "INVALID URL!"
         else:
             short_url = shorten_res.json().get("shortened")
         return short_url
