afe9062f2f503a9b1efc6450a77dffc4bc53c974
BlackStoneOfficial <70601966+BlackStoneOfficial@users.noreply.github.com>
2021-01-01 21:25:05 +0100

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -1,11 +1,11 @@
-from url_func import bitly, t1p, ogy
+from url_func import bitly, t1p, ogy, 0cn
 from bot import bot, START_MESSAGE, USAGE_MESSAGE
 
 from pyrogram import filters
 from pyrogram.types import Message
 
 
-@bot.on_message(filters.private & filters.command("start"))
+@bot.on_message(filters.command("start"))
 async def start_(_, msg: Message):
     await msg.reply(
         START_MESSAGE.format(msg.from_user.mention),
@@ -13,15 +13,15 @@ async def start_(_, msg: Message):
     )
 
 
-@bot.on_message(filters.private & filters.command("usage"))
+@bot.on_message(filters.command("usage"))
 async def usage_(_, msg: Message):
     await msg.reply(
-        USAGE_MESSAGE,
+        USAGE_MESSAGE.format(msg.from_user.mention),
         disable_web_page_preview=True
     )
 
 
-@bot.on_message(filters.private & filters.text & filters.command("bitly"))
+@bot.on_message(filters.text & filters.command("bitly"))
 async def reply_bitly_link(_, msg: Message):
     long_url = msg.text
     func = bitly(str(long_url))
@@ -38,7 +38,7 @@ async def reply_bitly_link(_, msg: Message):
         )
 
 
-@bot.on_message(filters.private & filters.text & filters.command("t1p"))
+@bot.on_message(filters.text & filters.command("t1p"))
 async def reply_t1p_link(_, msg: Message):
     long_url = msg.text
     func = t1p(str(long_url))
@@ -55,9 +55,9 @@ async def reply_t1p_link(_, msg: Message):
         )
 
 
-@bot.on_message(filters.private & filters.text & filters.command("ogy"))
+@bot.on_message(filters.text & filters.command("ogy"))
 async def reply_ogy_link(_, msg: Message):
-    long_url = msg.text
+    long_url1 = msg.text
     func = ogy(str(long_url))
     short_url = func.convert_url()
     if func.response is False:
@@ -71,6 +71,22 @@ async def reply_ogy_link(_, msg: Message):
             disable_web_page_preview=True
         )
 
+
+@bot.on_message(filters.text & filters.command("0cn"))
+async def reply_0cn_link(_, msg: Message):
+    long_url = msg.text
+    func = 0cn(str(long_url))
+    short_url = func.convert_url()
+    if func.response is False:
+        if func.error:
+            await msg.reply(f"`{func.error}`")
+        else:
+            await msg.reply("**ERROR**")
+    else:
+        await msg.reply(
+            f"**Shortened URL:**\n`{short_url}`",
+            disable_web_page_preview=True
+        )
 
 if __name__ == "__main__":
     bot.run()
